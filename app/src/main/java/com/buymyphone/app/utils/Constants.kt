de5a49ebc39ac9ef7c0bbf617365c252ee4b261b
rabbihossainltd1 <rabbixofficial@gmail.com>
2026-04-16 16:10:04 +0600

    Create Constants.kt

---
+++ app/src/main/java/com/buymyphone/app/utils/Constants.kt
@@ -0,0 +1,32 @@
+package com.buymyphone.app.utils
+
+object Constants {
+    const val EXTRA_ANALYSIS_RESULT = "extra_analysis_result"
+    const val EXTRA_REPORT_ID = "extra_report_id"
+    const val EXTRA_REPORT_TYPE = "extra_report_type"
+    const val EXTRA_SOFTWARE_RESULT = "extra_software_result"
+    const val EXTRA_HARDWARE_RESULT = "extra_hardware_result"
+    const val EXTRA_BUYSELL_RESULT = "extra_buysell_result"
+    const val EXTRA_DEVICE_INFO = "extra_device_info"
+
+    const val REPORT_TYPE_STANDARD = "STANDARD"
+    const val REPORT_TYPE_SOFTWARE = "SOFTWARE"
+    const val REPORT_TYPE_HARDWARE = "HARDWARE"
+    const val REPORT_TYPE_BUYSELL = "BUYSELL"
+
+    const val PDF_REPORTS_DIR = "BuyMyPhone_Reports"
+
+    const val REQUEST_CODE_CAMERA = 1001
+    const val REQUEST_CODE_AUDIO = 1002
+    const val REQUEST_CODE_STORAGE = 1003
+    const val REQUEST_CODE_ALL_PERMISSIONS = 1010
+
+    const val ANIMATION_DURATION_SCORE = 1500L
+    const val ANIMATION_DURATION_SHORT = 300L
+    const val ANIMATION_DURATION_MEDIUM = 600L
+
+    const val PREFS_NAME = "buymyphone_prefs"
+    const val KEY_DARK_MODE = "dark_mode"
+    const val KEY_PREMIUM = "is_premium"
+    const val KEY_REPORT_COUNT = "report_count"
+}
